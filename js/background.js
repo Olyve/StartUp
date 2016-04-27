561ea83a94dba7b4761b58c64dbfb90ee5b71c73
Sam <samgalizia@gmail.com>
2016-04-27 10:57:24 -0400

    Version 0.2.1a
    - Added CSS styling to options page
    - Modified background.js to work with message passing
    - Background.js now communicates with options page via message passing
    - Added new features to options page
    - Updated version number in manifest file
    - Added features to options page

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 // Global Variables
 var currentGroup;
-var groups;
+var groups = [];
 var options;
 
 // Short hand function to save the current data to storage
@@ -13,81 +13,94 @@ var saveData = function () {
   });
 }
 
-// On start query for saved data to make sure data is current
-chrome.storage.sync.get(function(items) {
-  // Check if there are groups
-  if (items['groups']) { // Set the groups
-    groups = items['groups'];
-  } else { // Create default group and add to list of groups
-    currentGroup = new Group('default', []);
-    groups.push(currentGroup);
-  }
+document.addEventListener('DOMContentLoaded', function () {
+  // On start query for saved data to make sure data is current
+  chrome.storage.sync.get(function(items) {
+    console.log(items);
+    // Check if there are groups
+    if (items['groups']) { // Set the groups
+      groups = items['groups'];
+    } else { // Create default group and add to list of groups
+      currentGroup = new Group('default', []);
+      groups.push(currentGroup);
+      saveData();
+    }
 
-  // Check for current group, if none set to first available group
-  if (items['currentGroup']) {
-    currentGroup = items['currentGroup'];
-  } else {
-    currentGroup = groups[0];
-  }
+    // Check for current group, if none set to first available group
+    if (items['currentGroup']) {
+      currentGroup = items['currentGroup'];
+    } else {
+      currentGroup = groups[0];
+      saveData();
+    }
 
-  // Check for the options
-  if (items['options']) {
-    options = items['options'];
-  }
+    if (items['options']) {
+      options = items['options'];
+    }
 
-  saveData();
-
-  // After data has been fetched bring up the tabs
-  chrome.tabs.query({'currentWindow': true}, function(tabs) {
-    for (var i = 0; i < currentGroup.urls.length; i++) {
-      if (options.overrideHomepages) {
-        if (tabs[i].url.length > 0) {
-          chrome.tabs.update(tabs[0].id, {'url': currentGroup.urls[i]});
-        } else {
+    // After data has been fetched bring up the tabs
+    chrome.tabs.query({'currentWindow': true}, function(tabs) {
+      for (var i = 0; i < currentGroup.urls.length; i++) {
+        if (options.overridePages === true) {
+          if (tabs[i].url.length > 0) {
+            chrome.tabs.update(tabs[0].id, {'url': currentGroup.urls[i]});
+          } else {
+            chrome.tabs.create({'url': currentGroup.urls[i]});
+          }
+        } else { // Don't override homepages or open tabs
           chrome.tabs.create({'url': currentGroup.urls[i]});
         }
-      } else { // Don't override homepages or open tabs
-        chrome.tabs.create({'url': currentGroup.urls[i]});
       }
-      currentGroup.urls[i]
+    }); // End tabs.query
+
+  }); // End storage.sync.get
+
+  // Add message listener
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
+    // addUrl
+    if (request.message === 'addUrl') {
+      console.log('Recieved message: ' + request.message);
+      // Check if the group contains the url already
+      if (Group.prototype.containsUrl.call(currentGroup, request.url) === false) {
+        Group.prototype.addUrl.call(currentGroup, request.url);
+        saveData();
+        sendResponse({'message': 'Saved ' + request.url});
+      }
     }
-  }); // End tabs.query
 
-}); // End storage.sync.get
+    // removeUrl
+    if (request.message === 'removeUrl') {
+      // Check if the group contains the url
+      if (Group.prototype.containsUrl.call(currentGroup, request.url)) {
+        Group.prototype.removeUrl.call(currentGroup, request.url);
+        saveData();
+        sendResponse({'message': 'Removed ' + request.url})
+      }
+    }
 
-// Add message listener
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
-  // addUrl
-  if (request.message === 'addUrl') {
-    console.log('Recieved message: ' + request.message);
-    // Check if the group contains the url already
-    if (Group.prototype.containsUrl.call(currentGroup, request.url) === false) {
-      Group.prototype.addUrl.call(currentGroup, request.url);
+    // clearAll
+    if (request.message === 'clearAll') {
+      Group.prototype.clearUrls.call(currentGroup);
       saveData();
-      sendResponse({'message': 'Saved ' + request.url});
+      sendResponse({'message': 'Urls have been cleared.'});
     }
-  }
 
-  // removeUrl
-  if (request.message === 'removeUrl') {
-    // Check if the group contains the url
-    if (currentGroup.containsUrl(request.url)) {
-      currentGroup.removeUrl(request.url);
-      saveData();
-      sendResponse({'message': 'Removed ' + request.url})
+    // getCurrentGroupName
+    if (request.message === 'getCurrentGroup') {
+      sendResponse({'group': currentGroup});
     }
-  }
 
-  // clearAll
-  if (request.message === 'clearAll') {
-    Group.prototype.clearUrls.call(currentGroup);
-    saveData();
-    sendResponse({'message': 'Urls have been cleared.'});
-  }
+    // changeName
+    if (request.message === 'changeName') {
+      if (request.newName) {
+        currentGroup.name = request.newName;
+        sendResponse({'message': 'Updated currentGroup.name'});
+      } else {
+        console.error('No new name was found in the requested message.');
+        sendResponse({'message': 'Could not update currentGroup.name. Please check background.js for more information.'});
+      }
+    }
 
-  // getCurrentGroupName
-  if (request.message === 'getCurrentGroupName') {
-    sendResponse({'name': currentGroup.name});
-  }
-});
+  }); // End of runtime.onMessage
+}); // End document.addEventListener
--- js/options.js
+++ js/options.js
@@ -1,36 +1,83 @@
+// Do this after page content has loaded
 document.addEventListener('DOMContentLoaded', function() {
   var options = {};
+  loadOptions();
 
-  // Check for options array containing options
-  chrome.storage.sync.get(function(items) {
-    options = items['options'];
-    console.log(options);
-
-    document.getElementById('overrideHomepages').checked = options.overrideHomepages;
+  // Get the current group and fill in details
+  chrome.runtime.sendMessage({'message': 'getCurrentGroup'}, function(response) {
+    var group = response.group;
+    document.getElementById('currentGroup').innerText = group.name;
+    displayUrls(group.urls);
   });
 
-  // Current Group
-  chrome.runtime.sendMessage({'message': 'getCurrentGroupName'}, function(response) {
-    document.getElementById('currentGroup').innerText = response.name;
-  });
-
+  /* Button Listeners */
 
   // Add listener to clearUrls
   document.getElementById('clearAll').addEventListener('click', function() {
     chrome.runtime.sendMessage({'message': 'clearAll'}, function(response) {
       console.log(response.message);
+      chrome.tabs.reload();
     });
   });
 
   // Add listener to saveChanges
   document.getElementById('saveChanges').addEventListener('click', function() {
-    options.overrideHomepages = document.getElementById('overrideHomepages').checked;
+    saveOptions();
+  });
 
-    console.log(options);
-    chrome.storage.sync.set({'options': options}, function() {
-      if (chrome.runtime.lastError) {
-        console.error('Could not save options: ' + chrome.runtime.lastError);
-      }
+  document.getElementById('changeName').addEventListener('click', function() {
+    var newName = document.getElementById('newName');
+    var name;
+
+    // Validate new name
+    if (newName.value.length > 0 && newName.value.length < 13) {
+      name = newName.value.trim();
+    }
+
+    // Tell background.js to update the name
+    chrome.runtime.sendMessage({'message': 'changeName', 'newName': name}, function(response) {
+      console.log(response.message);
+      chrome.tabs.reload();
     });
   });
-});
+
+}); // End of document.addEventListener
+
+/* Methods */
+
+// Load options method
+function loadOptions() {
+  // Check for options object containing options
+  chrome.storage.sync.get(function(items) {
+    // Check for the options object
+    if (items['options']) {
+      options = items['options'];
+
+      /* Set options on page. */
+      document.getElementById('overridePages').checked = options.overridePages;
+    }
+
+  });
+}
+
+// Save options method
+function saveOptions() {
+  options.overridePages = document.getElementById('overridePages').checked;
+  chrome.storage.sync.set({'options': options}, function() {
+    if (chrome.runtime.lastError) {
+      console.error('Could not save options: ' + chrome.runtime.lastError);
+    } else {
+      chrome.tabs.reload();
+    }
+  });
+}
+
+// Display group urls
+function displayUrls(list) {
+  var div = document.getElementById('urls');
+  for (var i = 0; i < list.length; i++) {
+    var p = document.createElement('p');
+    p.appendChild(document.createTextNode(list[i]));
+    div.appendChild(p);
+  }
+}
